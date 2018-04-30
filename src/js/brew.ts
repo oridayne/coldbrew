0b81fde55fe0b1e7e4761611dbe8353b397189b6
Aaron L. Zeng <me@bcc32.com>
2018-04-30 02:57:42 -0400

    Implement Package class

--- src/js/brew.ts
+++ src/js/brew.ts
@@ -1,8 +1,76 @@
 import "./navbar";
 import * as Util from "./util";
 
- // stack of package objects deleted. This helps with undo.
+class Package {
+    private firstname: string;
+    private lastname: string;
+    private packageNumber: string;
+    private location: string;
+    private carrier: string;
+    private comments: string | undefined;
+
+    constructor(firstname: string, lastname: string, packageNumber: string,
+                location: string, carrier: string, comments?: string) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.packageNumber = packageNumber;
+        this.location = location;
+        this.carrier = carrier;
+        this.comments = comments;
+    }
+
+    public static fromInputs(): Package {
+        // FIXME should fail if required inputs are not present
+
+        const firstname = Util.getInputValueById("firstname");
+        const lastname = Util.getInputValueById("lastname");
+        const packageNumber = Util.getInputValueById("packageNumber");
+
+        const location = Util.getInputValueById("location");
+        const carrier = Util.getInputValueById("carrier");
+
+        let comments: string | undefined = Util.getInputValueById("comments");
+        if (comments.length === 0) {
+            comments = undefined;
+        }
+
+        return new Package(
+            firstname,
+            lastname,
+            packageNumber,
+            location,
+            carrier,
+            comments,
+        );
+    }
+
+    public render(): HTMLLIElement {
+        const name = `${this.lastname}, ${this.firstname}`;
+        const popupText = `${this.location}, pkg. #${this.packageNumber}`;
+
+        const li = document.createElement("li");
+        li.classList.add("popup");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        li.appendChild(checkbox);
+
+        li.insertAdjacentText("beforeend", name);
+
+        const popup = document.createElement("span");
+        popup.classList.add("popuptext");
+        popup.textContent = popupText;
+        li.appendChild(popup);
+
+        return li;
+    }
+}
+
+// stack of package objects deleted. This helps with undo.
+// TODO make this a stack of Package
 const deletedPackages: HTMLLIElement[][] = [];
+
+const allPackages: Set<Package> = new Set();
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -13,7 +81,9 @@ document.addEventListener("DOMContentLoaded", () => {
     Util.getElementById("popExample").addEventListener("click", popUp);
     Util.getElementById("packageForm").addEventListener("submit", (e) => {
         e.preventDefault();
-        addPackage();
+        const p = Package.fromInputs();
+        allPackages.add(p);
+        addListItem(p.render());
         clearPackageInput();
     });
     Util.getElementById("clearPackageInput").addEventListener("click", clearPackageInput);
@@ -22,6 +92,9 @@ document.addEventListener("DOMContentLoaded", () => {
         addNote();
         clearNoteInput();
     });
+    // TODO implement search
+    // Util.getElementById("search").addEventListener("input", (e) => {
+    // });
 
 });
 
@@ -30,42 +103,6 @@ function clearPackageInput() {
     for (const input of inputs) {
         input.value = "";
     }
-}
-
-function addPackage() {
-    // FIXME should fail if required inputs are not present
-
-    const firstname = Util.getInputValueById("firstname");
-    const lastname = Util.getInputValueById("lastname");
-    const packageNumber = Util.getInputValueById("packageNumber");
-
-    const location = Util.getInputValueById("location");
-    // const carrier = getInputValueById("carrier");
-    // const comments = getInputValueById("comments");
-
-    const nameText = `${lastname}, ${firstname}`;
-    const popupText = `${location}, pkg. #${packageNumber}`;
-
-    const li = createListItem(nameText, popupText);
-    addListItem(li);
-}
-
-function createListItem(name: string, popupText: string): HTMLLIElement {
-    const li = document.createElement("li");
-    li.classList.add("popup");
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    li.appendChild(checkbox);
-
-    li.insertAdjacentText("beforeend", name);
-
-    const popup = document.createElement("span");
-    popup.classList.add("popuptext");
-    popup.textContent = popupText;
-    li.appendChild(popup);
-
-    return li;
 }
 
 // Given a list item, add it to its correct place (alphabetically) in the package list.
@@ -81,7 +118,6 @@ function addListItem(listItem: HTMLLIElement) {
         }
     }
     ol.appendChild(listItem);
-
 }
 
 function checkAll() {
